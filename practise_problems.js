a08b3902c293b7c829a76d0345790f5abc20c2d4
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-11-08 19:43:07 +0530

    created stargazing log

---
+++ practise_problems.js
@@ -0,0 +1,33 @@
+const blueCount = (currentCount, element) => {
+  const count = element === "blue" ? currentCount + 1 : currentCount;
+  return count;
+};
+
+const selectDistinct = (result, element) => {
+  if (!result.includes(element)) {
+    result.push(element);
+  }
+  return result;
+};
+
+const heading = function (text) {
+  return `\n${text}\n${"-".repeat(text.length)}\n`;
+};
+
+const festivalRibbonCount = function () {
+  console.log(heading("Festival Ribbon Count"));
+  console.log(["red", "blue", "red", "green", "red", "blue"].reduce(blueCount, 0));
+  console.log(["red", "blue", "red", "blue", "red", "blue"].reduce(blueCount, 0));
+};
+
+const stargazingLog = function () {
+  console.log(heading("Stargazing Log"));
+  console.log([["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]].flatMap(x => x).reduce(selectDistinct, []));
+};
+
+const main = function () {
+  festivalRibbonCount();
+  stargazingLog();
+};
+
+main();
