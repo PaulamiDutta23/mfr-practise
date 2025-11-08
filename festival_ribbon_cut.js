b9bc99058c1861cd8380cc4ca40869f61b4047b2
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-11-08 20:31:26 +0530

    changed

--- festival_ribbon_cut.js
+++
@@ -1,7 +0,0 @@
-const blueCount = (currentCount, element) => {
-  const count = element === "blue" ? currentCount + 1 : currentCount;
-  return count;
-}
-
-console.log(["red", "blue", "red", "green", "red", "blue"].reduce(blueCount, 0));
-console.log(["red", "blue", "red", "blue", "red", "blue"].reduce(blueCount, 0));
