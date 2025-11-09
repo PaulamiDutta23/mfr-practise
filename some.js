4b8ced625f16d45066e3c08ed00bf1e88d80c23c
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-11-09 13:25:15 +0530

    created Photography Exposure Values

--- some.js
+++ some.js
@@ -2,22 +2,27 @@ const heading = text => `\n${text}\n${"-".repeat(text.length)}\n`;
 
 const doesContainMusicalNote = data => {
   console.log(heading("Music Reharshal Note"));
-  console.log(data.flatMap(x => x).some(element => element.includes("do")));
+  console.log(data.flatMap(x => x).some(element => element === "do"));
 };
 
 const doesContainMusicSequence = data => {
   console.log(heading("Choir Harmony Review"));
-  console.log(data.flatMap(x => x).some(element => element.includes("so")));
+  console.log(data.flatMap(x => x).some(element => element === "so"));
 };
 
 const doesContainDanceStep = data => {
   console.log(heading("Dance Class Steps"));
-  console.log(data.flatMap(x => x).some(element => element.includes("turn")));
+  console.log(data.flatMap(x => x).some(element => element === "turn"));
 };
 
 const doesContainMeasurement = data => {
   console.log(heading("Fish Tank Measurement"));
-  console.log(data.flatMap(x => x).some(x => x > 7));
+  console.log(data.flatMap(x => x).some(element => element > 7));
+};
+
+const doesContainExposure = data => {
+  console.log(heading("Photography Exposure Values"));
+  console.log(data.flatMap(x => x).some(element => element === 4));
 };
 
 const main = () => {
@@ -25,6 +30,7 @@ const main = () => {
   doesContainMusicSequence([["la", "la"], ["mi"], ["so", "la"]]);
   doesContainDanceStep([["step", "tap"], ["turn", "step"]]);
   doesContainMeasurement([[5, 6], [7], [6]]);
+  doesContainExposure([2, 3], [1], [4, 2]);
 };
 
 main();
