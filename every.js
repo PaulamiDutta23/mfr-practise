1a56493f448eb5bf8e858b804b10b818d0f28022
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-11-09 16:23:16 +0530

    created Listed Age for SAREGAMAPA

--- every.js
+++ every.js
@@ -20,11 +20,17 @@ const doAllRecordContainBelow50 = data => {
   console.log(data.flatMap(x => x).every(element => element < 50));
 };
 
+const doAllRecordContain18OrAbove = data => {
+  console.log(heading("Listed Age for SAREGAMAPA"));
+  console.log(data.flatMap(x => x).every(element => element >= 18));
+};
+
 const main = () => {
   doAllTemperatureSheetContainBelow32([[22, 23], [25, 24, 22], [29]]);
   doAllDataContainPositive([[3, 4], [5, 2], [1]]);
   doAllReadingContainBelow120([[110, 115] ,[118], [109]]);
   doAllRecordContainBelow50([[30, 41] ,[51], [40]]);
+  doAllRecordContain18OrAbove([[18, 17] ,[21], [20, 32]]);
 };
 
 main();
