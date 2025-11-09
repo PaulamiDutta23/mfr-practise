831f6342ca712ccf89be41cbb268d871ca24f4c7
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-11-09 10:59:40 +0530

    created rainfall data check

--- every.js
+++ every.js
@@ -1,5 +1,7 @@
 const doAllLesser32 = element => element.every(x => x < 32);
 
+const doAllPositive = element => element.every(x => x > 0);
+
 const heading = function (text) {
   return `\n${text}\n${"-".repeat(text.length)}\n`;
 };
@@ -9,8 +11,14 @@ const doAllTemperatureSheetContainLesser32 = function () {
   console.log([[22, 23], [25, 24, 22], [29]].every(doAllLesser32));
 };
 
+const doAllDataContainPositive = function () {
+  console.log(heading("Rainfall Data Check"));
+  console.log([[3, 4], [5, 2], [1]].every(doAllPositive));
+};
+
 const main = function () {
   doAllTemperatureSheetContainLesser32();
+  doAllDataContainPositive();
 };
 
 main();
