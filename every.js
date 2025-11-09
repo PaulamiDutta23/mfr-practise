949564ac9fd61d2dc3e93601789d79d58fd4932a
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2025-11-09 15:50:20 +0530

    created Rainfall Records

--- every.js
+++ every.js
@@ -15,10 +15,16 @@ const doAllReadingContainBelow120 = data => {
   console.log(data.flatMap(x => x).every(element => element < 120));
 };
 
+const doAllRecordContainBelow50 = data => {
+  console.log(heading("Rainfall Records"));
+  console.log(data.flatMap(x => x).every(element => element < 50));
+};
+
 const main = () => {
   doAllTemperatureSheetContainBelow32([[22, 23], [25, 24, 22], [29]]);
   doAllDataContainPositive([[3, 4], [5, 2], [1]]);
   doAllReadingContainBelow120([[110, 115] ,[118], [109]]);
+  doAllRecordContainBelow50([[30, 41] ,[51], [40]]);
 };
 
 main();
